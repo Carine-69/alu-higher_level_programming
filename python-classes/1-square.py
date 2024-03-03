ce95a21d253e11c73514746c296f48a09613e2b1
Carine-69 <c.umugabeka@alustudent.com>
2024-03-03 10:23:14 -0800

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class:Square"""
+
+
+class Square:
+
+    """ class init """
+
+
+    def __init__(self,size):
+        '''initialize the rectangle size'''
+        self.__size = size
