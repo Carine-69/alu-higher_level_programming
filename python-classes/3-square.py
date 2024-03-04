a61ed16350bedf3573bcc6e121c258b9a1b3cc44
Carine-69 <c.umugabeka@alustudent.com>
2024-03-04 00:09:27 -0800

    task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin//python3
+"""create a square"""
+class square:
+    """instance att is size"""
+    def _init_(self,size=0)
+    if not instance(size, init)
+    raise typeError("size must an integer")
+elif size<0:
+    raise TypeError("size must be >=0")
+self._size=size
+def area(self):
+square.area=self._size**2
+return square_area
