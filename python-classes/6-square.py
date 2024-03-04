6e46dadd8fc833acac971f5c1f47193532512299
Carine-69 <c.umugabeka@alustudent.com>
2024-03-04 00:50:10 -0800

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -14,21 +14,40 @@ class Square:
 
     @property
     def size(self):
-        """Returns size."""
+        """returns size."""
         return self.__size
 
     @size.setter
     def size(self, size):
-        """Sets size to a size."""
+        """size to a size."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the data."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieves the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size to a value."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
     @property
     def position(self):
-        """Returns the position."""
+        """Retrieves the position."""
         return self.__position
 
     @position.setter
@@ -47,7 +66,7 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """Prints in stdout the square with the character #."""
+        """prints in stdout the square with the character #."""
         if self.__size == 0:
             print()
         else:
