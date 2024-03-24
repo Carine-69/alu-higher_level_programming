16b97515ff46edc2653ff9d28d9fdf3451052b70
Carine-69 <c.umugabeka@alustudent.com>
2024-03-24 11:35:57 -0700

    SQL joins

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
---list all the privileges of the MySQL users_0d_1 and user_0d_2 on your server (in locoalhost).
---cat 0_rpivileges.sql | mysql -lpocalhost -u root -p
+--  lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+-- cat 0-privileges.sql | mysql -hlocalhost -uroot -p
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
