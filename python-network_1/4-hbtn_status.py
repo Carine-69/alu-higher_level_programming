2fb3908a30e9cf7bd9a079fa6878ae21c1f01273
Carine-69 <c.umugabeka@alustudent.com>
2024-03-25 05:04:07 -0700

    #0

--- python-network_1/4-hbtn_status.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Python script that fetches data"""
-import requests
-
-if __name__ == "__main__":
-    res = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
